50a3ab2c0640554276a9b6ce3811b25f375bdcde
Fletcher Christian <fletcher@jamkillerproductions.io>
2025-04-19 22:04:23 -0500

    commit

--- DontKilltheJamNFTMojoClaimRedeax.sol
+++ DontKilltheJamNFTMojoClaimRedeax.sol
@@ -3,75 +3,80 @@ pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC721Base.sol";
 
-contract DKTJ is ERC721Base {
-    uint256 public constant MAX_SUPPLY = 10000;
+contract PleaseDontKillTheJam is ERC721Base {
+    uint256 public constant MAX_SUPPLY = 10_000;
     uint256 public mintFee;
 
     mapping(uint256 => uint256) public mojoScore;
-    mapping(uint256 => string) public narrative;
+    mapping(uint256 => string)  public narrative;
+
+    event NFTMinted(address indexed to, uint256 indexed tokenId, uint256 mojo, string narr);
 
     constructor(
         address _defaultAdmin,
         string memory _name,
         string memory _symbol,
         address _royaltyRecipient,
-        uint128 _royaltyBps
-    ) ERC721Base(_defaultAdmin, _name, _symbol, _royaltyRecipient, _royaltyBps) {}
-
-    function mintTo(
-        address to,
-        string memory _tokenURI,
-        uint256 _mojoScore,
-        string memory _narrative
-    ) external payable {
-        require(msg.value >= mintFee, "Insufficient mint fee");
-        require(totalSupply() < MAX_SUPPLY, "Max supply reached");
-        
-        uint256 tokenId = nextTokenIdToMint();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, _tokenURI);
-        
-        mojoScore[tokenId] = _mojoScore;
-        narrative[tokenId] = _narrative;
+        uint128 _royaltyBps,
+        uint256 _initialMintFee
+    )
+        ERC721Base(_defaultAdmin, _name, _symbol, _royaltyRecipient, _royaltyBps)
+    {
+        mintFee = _initialMintFee;
     }
 
-    function batchMintTo(
+    /// @notice Mint one NFT with mojo & narrative attached
+    function mint(
         address to,
-        string[] memory _tokenURIs,
-        uint256[] memory _mojoScores,
-        string[] memory _narratives
+        string calldata tokenURI,
+        uint256 mojo,
+        string calldata narr
     ) external payable {
-        require(
-            _tokenURIs.length == _mojoScores.length && 
-            _tokenURIs.length == _narratives.length,
-            "Array lengths must match"
-        );
-        require(
-            totalSupply() + _tokenURIs.length <= MAX_SUPPLY, 
-            "Batch would exceed max supply"
-        );
-        
-        require(msg.value >= mintFee * _tokenURIs.length, "Insufficient mint fee");
-        
-        for (uint256 i = 0; i < _tokenURIs.length; i++) {
-            uint256 tokenId = nextTokenIdToMint();
-            _safeMint(to, tokenId);
-            _setTokenURI(tokenId, _tokenURIs[i]);
-            
-            mojoScore[tokenId] = _mojoScores[i];
-            narrative[tokenId] = _narratives[i];
+        require(msg.value == mintFee,            "Bad mint fee");
+        require(totalSupply() < MAX_SUPPLY,      "Sold out");
+
+        uint256 id = nextTokenIdToMint();
+        _safeMint(to, id);
+        _setTokenURI(id, tokenURI);
+        mojoScore[id] = mojo;
+        narrative[id] = narr;
+
+        emit NFTMinted(to, id, mojo, narr);
+    }
+
+    /// @notice Mint multiple in one go. Fees must exactly equal count * mintFee.
+    function mintBatch(
+        address to,
+        string[] calldata tokenURIs,
+        uint256[] calldata mojos,
+        string[] calldata narrs
+    ) external payable {
+        uint256 count = tokenURIs.length;
+        require(count > 0,                        "No tokens");
+        require(count == mojos.length 
+             && count == narrs.length,           "Array mismatch");
+        require(totalSupply() + count <= MAX_SUPPLY, "Too many");
+        require(msg.value == mintFee * count,     "Bad total fee");
+
+        for (uint256 i = 0; i < count; i++) {
+            uint256 id = nextTokenIdToMint();
+            _safeMint(to, id);
+            _setTokenURI(id, tokenURIs[i]);
+            mojoScore[id] = mojos[i];
+            narrative[id] = narrs[i];
+            emit NFTMinted(to, id, mojos[i], narrs[i]);
         }
     }
 
-    function withdraw() external onlyOwner {
-        uint256 balance = address(this).balance;
-        require(balance > 0, "No funds to withdraw");
-        payable(owner()).transfer(balance);
+    /// @notice Owner can update fee
+    function setMintFee(uint256 fee) external onlyOwner {
+        mintFee = fee;
     }
 
-    // Optional: Allow setting mint fee
-    function setMintFee(uint256 _fee) external onlyOwner {
-        mintFee = _fee;
+    /// @notice Withdraw all ETH
+    function withdraw() external onlyOwner {
+        uint256 bal = address(this).balance;
+        require(bal > 0, "Nothing to withdraw");
+        payable(owner()).transfer(bal);
     }
 }
-
